42cf2ad83854a80424aeab42fbf7869d09959880
Depp-shi <946707789@qq.com>
2022-04-21 17:19:18 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
 <body>
 <div class="container">
     <div class="content">
-        <div class="title">Hello Laravel! - by amth</div>
+        <div class="title">Hello Laravel!-by amth</div>
     </div>
 </div>
 </body>
